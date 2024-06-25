b738ca82a1415ffcbf8b383eee6ae46a873a1870
Andrey Komissarov <anicometores@gmail.com>
2024-06-25 16:02:07 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicApplication;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Program for installation. Press any key to exit.");
+        Console.ReadKey();
+    }
+}
