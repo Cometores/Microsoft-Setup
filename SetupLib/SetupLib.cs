2eb06dfd168509f292a7361429e12705c4b951e4
Andrey Komissarov <anicometores@gmail.com>
2024-07-08 07:11:58 +0200

    Custom actions

---
+++ SetupLib/SetupLib.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SetupLib
+{
+    [RunInstaller(true)]
+    public partial class SetupLib : System.Configuration.Install.Installer
+    {
+        public SetupLib()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            MessageBox.Show("Custom action happens");
+            base.Install(stateSaver);
+        }
+    }
+}
